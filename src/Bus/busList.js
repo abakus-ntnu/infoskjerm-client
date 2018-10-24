37eaa41c56656d1689a302dad5e1a37cc667b0cb
Dennis Janzso <dennisjanzso@dhcp-10-22-1-238.wlan.ntnu.no>
2018-10-24 16:26:47 +0200

    dill

---
+++ src/Bus/busList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BusList extends Component{
+
+
+  render_list(){
+      <div>
+        <busListItem busNR =
+      </div>
+  }
+
+  render(){
+    return this.render_list();
+  }
+}
