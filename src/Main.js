d564fd631b56c0bc13c88c8f1a865ac3b6688262
AsmundH <aasmund.haugse@gmail.com>
2019-01-04 11:35:24 +0100

    Remove unnecessary imports

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import Events from './Events/Events';
-import BusinessEvents from './Events/BusinessEvents';
+// import BusinessEvents from './Events/BusinessEvents';
 import Time from './components/Time/index';
 import Bus from './Bus/Bus';
 
