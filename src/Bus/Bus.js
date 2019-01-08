b28ef11f47b97256706b6fead2673ac48dbd0ad0
AsmundH <aasmund.haugse@gmail.com>
2019-01-08 12:55:49 +0100

    Changed names of styling to be more readable

--- src/Bus/Bus.js
+++ src/Bus/Bus.js
@@ -30,14 +30,14 @@ class BusComponent extends Component {
 
       <div className="full-grid">
         <h1 className="bus-til-byen">Til Byen</h1>
-        <h2 className="bus-til-byen-glos">Gløshaugen</h2>
+        <h2 className="bus-til-byen-fra-glos">Gløshaugen</h2>
         <BusList departureList={data.to.glos} className="til-byen-glos" />
-        <h2 className="bus-til-byen-prof">Prof. Brochs Gate</h2>
+        <h2 className="bus-til-byen-fra-prof">Prof. Brochs Gate</h2>
         <BusList departureList={data.to.prof} className="til-byen-prof" />
         <h1 className="bus-fra-byen">Fra Byen</h1>
-        <h2 className="bus-fra-byen-glos">Gløshaugen</h2>
+        <h2 className="bus-til-glos-fra-byen">Gløshaugen</h2>
         <BusList departureList={data.from.glos} className="fra-byen-glos" />
-        <h2 className="bus-fra-byen-prof">Prof. Brochs Gate</h2>
+        <h2 className="bus-til-prof-fra-byen">Prof. Brochs Gate</h2>
         <BusList departureList={data.from.prof} className="fra-byen-prof" />
       </div>
     );
