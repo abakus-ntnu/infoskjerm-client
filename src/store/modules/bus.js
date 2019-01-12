2fe13ff176991251c5c3d71d7c64e7ec101e1dae
AsmundH <aasmund.haugse@gmail.com>
2019-01-12 01:17:42 +0100

    Small changes to redux

--- src/store/modules/bus.js
+++ src/store/modules/bus.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
+import initialState from '../initialState';
 
 const FETCH_BUS_BEGIN = 'FETCH_BUS_BEGIN';
 const FETCH_BUS_SUCCESS = 'FETCH_BUS_SUCCESS';
 const FETCH_BUS_FAILURE = 'FETCH_BUS_FAILURE';
 
 
-const busReducer = (state = { loading: false, data: [], error: false }, action = {}) => {
+const busReducer = (state = initialState.bus, action = {}) => {
   switch (action.type) {
     case FETCH_BUS_BEGIN:
       return Object.assign({}, state, { loading: true });
     case FETCH_BUS_SUCCESS:
-      console.log(action.data);
       return Object.assign({}, state, { data: action.bus, loading: false });
     case FETCH_BUS_FAILURE:
       return Object.assign({}, state, { error: true, loading: false });
@@ -37,7 +37,7 @@ export function fetchBusFailure(error) {
 export function fetchBus() {
   return (dispatch) => {
     dispatch(fetchBusBegin());
-    axios.get('https://infoskjerm-api.koskom.no/bus') // Should be URL to bus-api
+    axios.get('https://infoskjerm-api.koskom.no/bus')
       .then((response) => {
         dispatch(fetchBusSuccess(response.data));
       })
