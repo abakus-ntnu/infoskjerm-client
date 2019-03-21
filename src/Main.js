a912f846bd84d7af0452b107a7d91b274572a339
AsmundH <aasmund.haugse@gmail.com>
2019-03-21 17:32:10 +0100

    Init setup for animation

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 import Time from './components/Time/index';
 import Bus from './Bus/Bus';
 import Events from './Events/Events';
 import SignUpEvents from './Events/SignUpEvents';
 import Abakus from './components/Abakus';
 
+const firstChild = (props) => {
+  const childrenArray = React.Children.toArray(props.children);
+  return childrenArray[0] || null;
+};
 class Main extends Component {
   render() {
     return (
