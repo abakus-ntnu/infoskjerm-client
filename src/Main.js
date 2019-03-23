e75b17b797e902e4b17fdef1bbeeb5c4986e0a2e
AsmundH <aasmund.haugse@gmail.com>
2019-03-23 14:59:49 +0100

    Fixing transition animations

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { CSSTransitionGroup } from 'react-transition-group';
 import Time from './components/Time/index';
 import Bus from './Bus/Bus';
 import Events from './Events/Events';
 import SignUpEvents from './Events/SignUpEvents';
 import Abakus from './components/Abakus';
 
-const firstChild = (props) => {
-  const childrenArray = React.Children.toArray(props.children);
-  return childrenArray[0] || null;
-};
 class Main extends Component {
   render() {
     return (
@@ -18,31 +15,10 @@ class Main extends Component {
           <div>
             <Time displayTime />
           </div>
-          <Route
-            exact
-            path="/"
-            children={({ match, ...rest }) => (
-              <TransitionGroup component={Events}>
-                {match && <Events {...rest} />}
-              </TransitionGroup>
-            )}
-          />
-          <Route
-            path="/events"
-            children={({ match, ...rest }) => (
-              <TransitionGroup component={Events}>
-                {match && <Events {...rest} /> }
-              </TransitionGroup>
-            )}
-          />
-          <Route
-            path="/bus"
-            children={({ match, ...rest }) => (
-              <TransitionGroup component={Bus}>
-                {match && <Bus {...rest} /> }
-              </TransitionGroup>
-            )}
-          />
+          <Route exact path="/" component={() => <Redirect to="/events" />} />
+          <Route path="/events" component={Events} />
+          <Route path="/bus" component={Bus} />
+          <Route path="/signup" component={SignUpEvents} />
           <Abakus />
         </div>
       </Router>
