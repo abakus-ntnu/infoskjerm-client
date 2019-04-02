00ad9ff9e7d6bf087214ea02e567a65df40c02ef
eirsture <eirik.sture@gmail.com>
2019-04-02 19:07:04 +0200

    copied over date and time formatting functions from SinglePromotedEvent

--- src/Events/UpcomingSingleEvent.js
+++ src/Events/UpcomingSingleEvent.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import './UpcomingEvents.css';
 
+const dateToFormattedHours = (date) => {
+  const hours = date.getHours();
+  return hours.toString().length <= 1 ? `0${hours}` : hours;
+};
+
+const dateToFormattedMinutes = (date) => {
+  const minutes = date.getMinutes();
+  return minutes.toString().length <= 1 ? `0${minutes}` : minutes;
+};
+
+const dateToFormattedTime = (isoString) => {
+  const date = new Date(isoString);
+  let timeString = '';
+  timeString += `${dateToFormattedHours(date)}:`;
+  timeString += `${dateToFormattedMinutes(date)}`;
+  return timeString;
+};
+
+const dateToMonth = (date) => {
+  const months = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember'];
+  return months[date.getMonth()];
+};
+
+const dateToFormattedDate = (isoString) => {
+  const date = new Date(isoString);
+  const formattedDate = `${date.getDate()}. ${dateToMonth(date)}`;
+  return formattedDate;
+};
+
 function getCapacity(totalCapacity, registrationCount) {
   return (totalCapacity === 0 ? 'Ingen påmelding!' : `${registrationCount}/${totalCapacity} påmeldt`);
 }
