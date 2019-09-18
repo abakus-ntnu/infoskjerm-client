1558c8ed3d35509ce0eaaa376eeec6a3fc49394e
eirsture <eirik.sture@gmail.com>
2019-09-18 18:14:28 +0200

    updated bus api

--- src/Bus/Bus.js
+++ src/Bus/Bus.js
@@ -30,15 +30,15 @@ class BusComponent extends Component {
       <div className="full-grid">
         <h1 id="bus-til-byen">Til Byen</h1>
         <h2 id="bus-til-byen-fra-glos" className="stopTitle">Gløshaugen</h2>
-        <BusList departureList={data.to.glos} className="bus-list" id="til-byen-fra-glos" />
+        <BusList departureList={data.to.gloshaugen} className="bus-list" id="til-byen-fra-glos" />
         <h2 id="bus-til-byen-fra-prof" className="stopTitle">Prof. Brochs Gate</h2>
-        <BusList departureList={data.from.glos} className="bus-list" id="fra-byen-til-glos" />
+        <BusList departureList={data.from.gloshaugen} className="bus-list" id="fra-byen-til-glos" />
 
         <h1 id="bus-fra-byen">Fra Byen</h1>
         <h2 id="bus-til-glos-fra-byen" className="stopTitle">Gløshaugen</h2>
-        <BusList departureList={data.to.prof} className="bus-list" id="til-byen-fra-prof" />
+        <BusList departureList={data.to.hesthagen} className="bus-list" id="til-byen-fra-prof" />
         <h2 id="bus-til-prof-fra-byen" className="stopTitle">Prof. Brochs Gate</h2>
-        <BusList departureList={data.from.prof} className="bus-list" id="fra-byen-til-prof" />
+        <BusList departureList={data.from.hesthagen} className="bus-list" id="fra-byen-til-prof" />
       </div>
     );
   }
